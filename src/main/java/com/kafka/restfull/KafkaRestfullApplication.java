436adc3ca350541c97b1ff769e7242959425c570
sabaja <jacoposabatini76@gmail.com>
2019-08-06 17:01:13 +0200

    UPLOAD PROJECT

---
+++ src/main/java/com/kafka/restfull/KafkaRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRestfullApplication.class, args);
+	}
+
+}
